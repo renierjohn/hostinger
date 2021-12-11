99cc53497fb8ccb6fec33768f7c9b76f86f8ac4c
Rendroid <renierjohnsediego@gmail.com>
2021-12-11 22:59:15 +0800

    add route detail Page

--- themes/Renify/js/main.js
+++ themes/Renify/js/main.js
@@ -158,6 +158,40 @@
             ]
         });
 
+        $('.thumb__slider').slick({
+            arrows: false,
+            dots: false,
+            infinite: true,
+            centerMode: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            pauseOnFocus: false,
+            autoplaySpeed: 1500,
+            responsive: [
+                {
+                    breakpoint: 1401,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 1101,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 701,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+
         $('.product-list').slick({
             arrows: false,
             dots: true,
