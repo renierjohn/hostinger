5e83314f2a661e5d70f9594ad2ac890bbbce0825
Rendroid <renierjohnsediego@gmail.com>
2022-02-10 21:22:00 +0800

    fix render

--- themes/Renify/js/main.js
+++ themes/Renify/js/main.js
@@ -30,9 +30,10 @@
         // $WIN.on('load', function() {
         $(document).ready(function() {
             //force page scroll position to top at page refresh
-            $('html, body').animate({ scrollTop: 0 }, 'normal');
+            // $('html, body').animate({ scrollTop: 0 }, 'normal');
 
             // will first fade out the loading animation
+            $("body").show();
             $("#loader").fadeOut("slow", function() {
                 // will fade out the whole DIV that covers the website.
                 $("#preloader").delay(300).fadeOut("slow");
@@ -41,7 +42,6 @@
             // for hero content animations
             $("html").removeClass('ss-preload');
             $("html").addClass('ss-loaded');
-            $("body").show();
         });
     };
 
