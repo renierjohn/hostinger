4327db9ce446b91e8882a6fcf6c23ed13abd2c74
renierjohn <reniersediego@prometsource.com>
2023-06-02 16:43:39 +0800

    ALetr menu add contetnt type.

--- themes/rboot/js/rboot.js
+++ themes/rboot/js/rboot.js
@@ -2,16 +2,30 @@
  * @file
  * renify_newgen behaviors.
  */
-(function (Drupal) {
+(function ($,Drupal) {
 
   'use strict';
 
   Drupal.behaviors.renifyNewgen = {
     attach: function (context, settings) {
+      var toggleButton = $('.header-menu-toggle'),
+      nav = $('.header-nav-wrap');
 
-      console.log('It works!');
+      toggleButton.on('click', (e) => {
+        e.preventDefault();
+        if (!toggleButton.hasClass('menu-flag')) {
+          toggleButton.toggleClass('is-clicked');
+          toggleButton.toggleClass('menu-flag');
+          nav.slideToggle(()=>{
+             toggleButton.toggleClass('menu-flag');
+           });
+        }
+      });
 
+      if (toggleButton.is(':visible')){
+         nav.addClass('mobile')
+       }
     }
   };
 
-} (Drupal));
+} (jQuery, Drupal));
