a4916bf51f63eca357a6957e080dea634d729d97
renierjohn <reniersediego@prometsource.com>
2023-09-18 17:40:06 +0800

    Add file.

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * The PHP page that serves all page requests on a Drupal installation.
+ *
+ * All Drupal code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
+ */
+
+use Drupal\Core\DrupalKernel;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Dotenv\Dotenv;
+
+$autoloader = require_once 'autoload.php';
+
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/.env');
+
+$kernel = new DrupalKernel('prod', $autoloader);
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
