de69ff2e31e54f30ea4978b95b1dea00eb647111
Renier John Sediego <reniersediego@prometsource.com>
2024-02-11 20:02:22 +0800

    D10 fix.

--- modules/custom/DataRouter/src/EventSubscriber/ResponseHook.php
+++ modules/custom/DataRouter/src/EventSubscriber/ResponseHook.php
@@ -12,8 +12,6 @@ use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\Event\FinishRequestEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Cookie;
-use Symfony\Component\HttpKernel\Event\RequestEvent;
-use Symfony\Component\HttpKernel\Event\ResponseEvent;
 
 /**
  * Class .
